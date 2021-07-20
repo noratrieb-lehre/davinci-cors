e1bd5b73878a18ab326c3fbe1ee877b960a667e1
nils <48135649+Nilstrieb@users.noreply.github.com>
2021-07-20 11:43:23 +0200

    class sql

--- rust/server/src/actions/class.rs
+++ rust/server/src/actions/class.rs
@@ -1,12 +1,41 @@
 use crate::actions::DbResult;
-use crate::models::{Class, NewClass};
+use crate::diesel::{QueryDsl, RunQueryDsl};
+use crate::models::{Class, Member, MemberRole, NewClass, User};
 use crate::Pool;
-use diesel::{insert_into, RunQueryDsl};
+use diesel::{delete, insert_into, update, BoolExpressionMethods, ExpressionMethods};
+use uuid::Uuid;
 
-pub fn insert_class(db: &Pool, new_class: NewClass) -> DbResult<()> {
+pub fn insert_class(db: &Pool, new_class: NewClass) -> DbResult<Class> {
     use crate::schema::class::dsl::*;
     let conn = db.get()?;
 
-    insert_into(class).values(&new_class).execute(&conn)?;
-    Ok(())
+    Ok(insert_into(class).values(&new_class).get_result(&conn)?)
+}
+
+type ClassMemberData = (Class, Vec<(Member, MemberRole)>);
+
+pub fn get_class(db: &Pool, class_id: Uuid) -> DbResult<Option<ClassMemberData>> {
+    use crate::schema::class::dsl::{class, id};
+    use crate::schema::member::dsl::member;
+    use crate::schema::member_role::dsl::member_role;
+    let conn = db.get()?;
+
+    let vec: Vec<(Class, (Member, MemberRole))> = class
+        .filter(id.eq(class_id))
+        .inner_join(member.inner_join(member_role))
+        .load(&conn)?;
+
+    Ok(map_class_join_members(vec))
+}
+
+fn map_class_join_members(vec: Vec<(Class, (Member, MemberRole))>) -> Option<ClassMemberData> {
+    match vec
+        .into_iter()
+        .fold((None, vec![]), |(_, mut vec), (class, member)| {
+            vec.push(member);
+            (Some(class), vec)
+        }) {
+        (Some(class), vec) => Some((class, vec)),
+        (None, _) => None,
+    }
 }
--- rust/server/src/handlers/class.rs
+++ rust/server/src/handlers/class.rs
@@ -1,5 +1,5 @@
 use crate::handlers::HttpResult;
-use actix_web::{web, HttpResponse};
+use actix_web::web;
 
 pub(super) fn class_config(cfg: &mut web::ServiceConfig) {
     cfg.route("/classes", web::post().to(create_class)).service(
